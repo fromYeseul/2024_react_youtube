7330abb682411a26d9bd6a3037c2afa7cf71df57
yeseul <from.yeseul@gmail.com>
2024-06-20 11:20:14 +0900

    페이지 SEO작업

--- src/pages/Channel.jsx
+++ src/pages/Channel.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import Main from '../components/section/Main'
 
 const Channel = () => {
-  return (
-    <div>Channel</div>
-  )
+    return (
+        <Main
+            title='웹스토리보이 유튜브' 
+            description='웹스토리보이 유튜브에 오신것을 환영합니다.'
+        >
+            Channel
+        </Main>
+    )
 }
 
 export default Channel
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import Main from '../components/section/Main'
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
+    return (
+        <Main 
+            title='웹스토리보이 유튜브' 
+            description='웹스토리보이 유튜브에 오신것을 환영합니다.'>
+            Home
+        </Main>
+    )
 }
 
 export default Home
