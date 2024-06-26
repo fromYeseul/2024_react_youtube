e007e066a2d3f3fa4786f887ea1a88f35494a966
yeseul <from.yeseul@gmail.com>
2024-06-26 16:58:03 +0900

    비디오 컴포넌트 통합작업

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,11 +2,17 @@ import React from 'react'
 import Main from '../components/section/Main'
 import Today from '../components/contents/Today'
 import Developer from '../components/contents/Developer'
-import Webd from '../components/contents/Webd'
-import Website from '../components/contents/Website'
-import Gsap from '../components/contents/Gsap'
-import Portfolio from '../components/contents/Portfolio'
-import Youtube from '../components/contents/Youtube'
+import VideoSlider from '../components/video/VideoSlider'
+import { webdText } from '../data/webd'
+import { websiteText } from '../data/website'
+import { gsapText } from '../data/gsap'
+import { portfolioText } from '../data/portfolio'
+import { youtubeText } from '../data/youtube'
+// import Webd from '../components/contents/Webd'
+// import Website from '../components/contents/Website'
+// import Gsap from '../components/contents/Gsap'
+// import Portfolio from '../components/contents/Portfolio'
+// import Youtube from '../components/contents/Youtube'
 
 const Home = () => {
     return (
@@ -16,11 +22,16 @@ const Home = () => {
             Home            
             <Today />
             <Developer />
-            <Webd />
+            <VideoSlider videos={webdText} title='웹디기능사 준비는 이걸로!' id='webd'/>
+            <VideoSlider videos={websiteText} title='웹사이트 준비는 이걸로!' id='website'/>
+            <VideoSlider videos={gsapText} title='gsap' id='gsap'/>
+            <VideoSlider videos={portfolioText} title='portfolio' id='portfolio'/>
+            <VideoSlider videos={youtubeText} title='youtube' id='youtube'/>
+            {/* <Webd />
             <Website />
             <Gsap />
             <Portfolio />
-            <Youtube />
+            <Youtube /> */}
         </Main>
     )
 }
