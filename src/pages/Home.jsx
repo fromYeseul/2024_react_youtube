6b29947a15ca8d233086dddccdda0d2b898c18e9
yeseul <from.yeseul@gmail.com>
2024-06-27 10:55:05 +0900

    swiper 이미지 슬라이드 작업

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,6 +8,8 @@ import { websiteText } from '../data/website'
 import { gsapText } from '../data/gsap'
 import { portfolioText } from '../data/portfolio'
 import { youtubeText } from '../data/youtube'
+import { developerText } from '../data/developer'
+import { todayText } from '../data/today'
 // import Webd from '../components/contents/Webd'
 // import Website from '../components/contents/Website'
 // import Gsap from '../components/contents/Gsap'
@@ -20,8 +22,8 @@ const Home = () => {
             title='웹스토리보이 유튜브'
             description='웹스토리보이 유튜브에 오신것을 환영합니다.'>
             Home            
-            <Today />
-            <Developer />
+            <Today videos={todayText} id='today' />
+            <Developer videos={developerText} title='추천개발자를 소개합니다.' id='developer' />
             <VideoSlider videos={webdText} title='웹디기능사 준비는 이걸로!' id='webd'/>
             <VideoSlider videos={websiteText} title='웹사이트 준비는 이걸로!' id='website'/>
             <VideoSlider videos={gsapText} title='gsap' id='gsap'/>
