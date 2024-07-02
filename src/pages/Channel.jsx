17702be645f17c52198e0ca81f5d04c3cabeea37
yeseul <from.yeseul@gmail.com>
2024-07-02 17:32:23 +0900

    채널 상세페이지 만들기

--- src/pages/Channel.jsx
+++ src/pages/Channel.jsx
@@ -1,13 +1,79 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { fetchFromAPI } from '../utils/api'
 import Main from '../components/section/Main'
 
+import { CiBadgeDollar } from "react-icons/ci";
+import { CiMedal } from "react-icons/ci";
+import { CiRead } from "react-icons/ci";
+import VideoSearch from '../components/videos/VideoSearch';
+
 const Channel = () => {
+    const {channelId} = useParams();
+    const [channelDetail, setChannelDetail] = useState();
+    const [channelVideo, setChannelVideo] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [nextPageToken, setNextPageToken] = useState(null);
+
+    useEffect(() => {
+        const fetchResults = async() =>{
+            try{
+                const data = await fetchFromAPI(`channels?part=snippet&id=${channelId}`)
+                setChannelDetail(data.items[0]);
+                
+                const videosData = await fetchFromAPI(`search?channelId=${channelId}&part=snippet%2Cid&order=date`);
+                setChannelVideo(videosData?.items);
+                setNextPageToken(videosData?.items);
+            }catch(error){
+                console.log('Error fetching data', error)
+            }finally{
+                setLoading(false);
+            }
+        }
+        fetchResults();
+    }, [channelId])
+
+    const loadMoreVideos = async () => {
+        if (nextPageToken) {
+            const videosData = await fetchFromAPI(`search?channelId=${channelId}&part=snippet%2Cid&order=date&pageToken=${nextPageToken}`);
+            setChannelVideo(prevVideos => [...prevVideos, ...videosData.items]);
+            setNextPageToken(videosData?.nextPageToken);
+        }
+    };
+
+    const channelPageClass = loading ? 'isLoading' : 'isLoaded';
+
     return (
         <Main
             title='웹스토리보이 유튜브' 
-            description='웹스토리보이 유튜브에 오신것을 환영합니다.'
-        >
-            Channel
+            description='웹스토리보이 유튜브에 오신것을 환영합니다.'>
+
+            <section id='channel' className={channelPageClass}>
+                {channelDetail && (
+                    <div className='channel__inner'>
+                        <div className='channel__header' style={{ backgroundImage: `url(${channelDetail.brandingSettings.image.bannerExternalUrl})` }}>
+                            <div className='circle'>
+                                <img src={channelDetail.snippet.thumbnails.high.url} alt={channelDetail.snippet.title} />
+                            </div>
+                        </div>
+                        <div className='channel__info'>
+                            <h3 className='title'>{channelDetail.snippet.title}</h3>
+                            <p className='desc'>{channelDetail.snippet.description}</p>
+                            <div className='info'>
+                                <span><CiBadgeDollar />{channelDetail.statistics.subscriberCount}</span>
+                                <span><CiMedal />{channelDetail.statistics.videoCount}</span>
+                                <span><CiRead />{channelDetail.statistics.viewCount}</span>
+                            </div>
+                        </div>
+                        <div className='channel__video video__inner search'>
+                            <VideoSearch videos={channelVideo}/>
+                        </div>
+                        <div className='channel__more'>
+                            {nextPageToken && <button onClick={loadMoreVideos}>더 보기</button>}
+                        </div>
+                    </div>
+                )}
+            </section>
         </Main>
     )
 }
